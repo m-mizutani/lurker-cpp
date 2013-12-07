9fae0674fb2faad363f3bdcf1d8d7f794483bfac
Masa Mizutani <mizutani@sfc.wide.ad.jp>
2013-12-07 23:35:31 +0900

    change build framework from waf to cmake

--- src/lurker.cc
+++ src/lurker.cc
@@ -2,8 +2,8 @@
 
 
 
-int main() {
-  swarm::CapPcapDev dev("en1");
+int main(int argc, char *argv[]) {
+  swarm::CapPcapDev dev(argv[1]);
   swarm::NetDec nd;
   dev.bind_netdec(&nd);
   dev.start();
